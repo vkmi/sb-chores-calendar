ec74f57b55281866a38a0c35685a579548f5e020
Michele Vittori <michele.vittori@gmail.com>
2021-05-05 05:55:18 +0200

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarViewComponent } from './container/calendar-view/calendar-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
