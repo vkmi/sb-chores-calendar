ecbedb3ce8e3de009c385606090fc96c583ffc65
Michele Vittori <michele.vittori@gmail.com>
2021-05-08 00:07:16 +0200

    implemented ordered execution of requests with delay and action on complete

--- src/app/_services/manga.service.ts
+++ src/app/_services/manga.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -20,9 +21,26 @@ export class MangaService {
       { headers: {} }
     );
   }
-  getMangaByTitle(title: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/manga?title=${title}`);
+  getMangaByTitle(title: string): Observable<Manga[]> {
+    return this.http.get<MangaResult>(`${this.baseUrl}/manga?title=${title}`).pipe(map(res => res.results));
   }
+  getChapterById(id: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/chapter/${id}`);
+  }
+
+  compareChapters(a, b) {
+    if (Number(a.data.attributes.chapter) > Number(b.data.attributes.chapter)) return 1;
+    if (Number(a.data.attributes.chapter) < Number(b.data.attributes.chapter)) return -1;
+    return 0;
+  }
+}
+
+export interface MangaResult {
+  results: Manga[];
+}
+export interface Manga {
+  relationships: { id: string; type: string }[];
+  data: {attributes:{title: {en:string}}};
 }
 
 export interface LoginResponse {
--- src/app/container/api-test/api-test.component.ts
+++ src/app/container/api-test/api-test.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { MangaService } from 'src/app/_services/manga.service';
+import { of } from 'rxjs';
+import { concatAll, concatMap, delay } from 'rxjs/operators';
+import { Manga, MangaService } from 'src/app/_services/manga.service';
 
 @Component({
   selector: 'app-api-test',
@@ -11,6 +13,8 @@ export class ApiTestComponent implements OnInit {
   password: string = '';
   title = '';
   chapters = [];
+  matches: Manga[];
+
   isLoggedIn = false;
 
   constructor(private mangadex: MangaService) {}
@@ -27,9 +31,48 @@ export class ApiTestComponent implements OnInit {
 
   searchManga() {
     this.mangadex.getMangaByTitle(this.title).subscribe((res) => {
-      this.chapters = res?.results[0]?.relationships?.filter(
-        (r: { type: string }) => r.type == 'chapter'
-      ).map((c: { id: any; }) => c.id);
+      this.matches = res;
     });
+  }
+
+  async getMangaChapters(index) {
+    this.chapters = [];
+
+    // const chaptersIds = this.matches[i].relationships
+    //   ?.filter((r) => r.type == 'chapter')
+    //   .map((c) => c.id);
+    const chaptersObs = this.matches[index].relationships
+      ?.filter((r) => r.type == 'chapter')
+      .map((c) => this.mangadex.getChapterById(c.id));
+
+    let i = 0;
+    of([...chaptersObs])
+      .pipe(
+        concatAll(),
+        concatMap((x) => of(x).pipe(delay(200)))
+      )
+      .subscribe((res) =>
+        res.subscribe((r) => {
+          console.log('got chapter', r);
+          this.chapters = [...this.chapters, r];
+          i++;
+          if (i == chaptersObs.length) {
+            this.chapters.sort(this.mangadex.compareChapters);
+            console.log(this.chapters.map((c) => c.data.attributes.chapter));
+          }
+        })
+      );
+
+    // for (const [index, id] of chaptersIds.entries()) {
+
+    //   this.mangadex.getChapterById(id).subscribe((res) => {
+    //     console.log('got chapter', index, res);
+    //     this.chapters = [...this.chapters, res];
+    //     if (index === chaptersIds.length - 1) {
+    //       this.chapters.sort(this.mangadex.compareChapters);
+    //       console.log(this.chapters.map((c) => c.data.attributes.chapter));
+    //     }
+    //   });
+    // }
   }
 }
