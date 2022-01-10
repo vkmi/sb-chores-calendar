bb25f97aac3bf16bab3210096a80e5dc863a86f8
vkmi <vkmi@users.noreply.github.com>
2022-01-10 10:51:42 +0100

    test correzione creazione calendario

--- src/app/classes/calendar.ts
+++ src/app/classes/calendar.ts
@@ -2,7 +2,7 @@ export class Calendar {
   static get thisAndNextYear(): Date[] {
     let dates: Date[] = new Array(365 * 2);
     const thisYear = new Date().getFullYear();
-    const firstDay = new Date(thisYear, 0, 1);
+    const firstDay = new Date(thisYear, 0, 1, 0,0,0,0);
     for (let i = 0; i < dates.length; i++) {
       dates[i] = new Date(firstDay.getTime() + 1000 * 60 * 60 * 24 * Number(i));
     }
